61fe283ce0ccb7f07c2f07e967f32c12c1156c42
lprimak <lprimak@localhost>
2011-08-28 02:37:48 -0400

    graceful degradation with @AJAX annotation

--- services/src/main/java/com/flowlogix/web/services/ServicesModule.java
+++ services/src/main/java/com/flowlogix/web/services/ServicesModule.java
@@ -4,11 +4,15 @@
  */
 package com.flowlogix.web.services;
 
+import com.flowlogix.web.services.internal.AjaxAnnotationWorker;
 import org.apache.tapestry5.ioc.Configuration;
+import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.annotations.Contribute;
+import org.apache.tapestry5.ioc.annotations.Primary;
 import org.apache.tapestry5.ioc.annotations.SubModule;
 import org.apache.tapestry5.services.ComponentClassResolver;
 import org.apache.tapestry5.services.LibraryMapping;
+import org.apache.tapestry5.services.transform.ComponentClassTransformWorker2;
 
 /**
  *
@@ -22,4 +26,13 @@ public class ServicesModule
     {
         configuration.add(new LibraryMapping("flowlogix", "com.flowlogix.web"));
     }
+    
+        
+    @Contribute(ComponentClassTransformWorker2.class)
+    @Primary
+    public static void provideClassTransformWorkers(OrderedConfiguration<ComponentClassTransformWorker2> configuration)
+    {
+        configuration.addInstance("AJAX", AjaxAnnotationWorker.class, "before:Property");
+    }
+
 }
---
+++ services/src/main/java/com/flowlogix/web/services/internal/AjaxAnnotationWorker.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flowlogix.web.services.internal;
+
+import com.flowlogix.web.services.annotations.AJAX;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.model.MutableComponentModel;
+import org.apache.tapestry5.plastic.MethodAdvice;
+import org.apache.tapestry5.plastic.MethodInvocation;
+import org.apache.tapestry5.plastic.PlasticClass;
+import org.apache.tapestry5.plastic.PlasticMethod;
+import org.apache.tapestry5.services.ComponentSource;
+import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.transform.ComponentClassTransformWorker2;
+import org.apache.tapestry5.services.transform.TransformationSupport;
+
+/**
+ *
+ * @author lprimak
+ */
+public class AjaxAnnotationWorker implements ComponentClassTransformWorker2
+{
+    public AjaxAnnotationWorker(Request request)
+    {
+        this.request = request;
+    }
+
+    
+    @Override
+    public void transform(final PlasticClass plasticClass, TransformationSupport support, MutableComponentModel model)
+    {
+        for(final PlasticMethod method : plasticClass.getMethodsWithAnnotation(AJAX.class))
+        {
+            if(method.isVoid() == false)
+            {
+                method.addAdvice(new MethodAdvice() 
+                {
+                    @Override
+                    public void advise(MethodInvocation invocation)
+                    {
+                        invocation.proceed();
+                        Object result = invocation.getReturnValue();
+                        if (!request.isXHR())
+                        {
+                            result = defaultForReturnType(result.getClass());
+                        }
+                        else
+                        {
+                            cs.getActivePage().getComponentResources().discardPersistentFieldChanges();
+                        }
+                        invocation.setReturnValue(result);
+                    }
+                });
+            }
+            else
+            {
+                throw new RuntimeException(
+                "@AJAX can be applied to non-void event handlers only"); 
+            }
+        }        
+    } 
+
+    
+
+    private Object defaultForReturnType(Class<?> returnType)
+    {
+        if (!returnType.isPrimitive())
+        {
+            return null;
+        }
+        if (returnType.equals(boolean.class))
+        {
+            return false;
+        }
+        return 0;
+    }
+    
+    
+    private final Request request;
+    private @Inject ComponentSource cs;
+}

7fd70cdf50d0a1247844a74f273599c6511696a7
lprimak <lprimak@localhost>
2011-09-29 23:51:34 -0400

    moved invalid auth delay in here

--- services/src/main/java/com/flowlogix/web/components/security/LoginFormBase.java
+++ services/src/main/java/com/flowlogix/web/components/security/LoginFormBase.java
@@ -4,11 +4,13 @@
  */
 package com.flowlogix.web.components.security;
 
+import com.flowlogix.web.services.SecurityModule;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import lombok.SneakyThrows;
 import org.apache.shiro.ShiroException;
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.util.SavedRequest;
@@ -33,7 +35,7 @@ import org.tynamo.security.services.SecurityService;
  */
 public class LoginFormBase
 {
-    @SneakyThrows(MalformedURLException.class)
+    @SneakyThrows({MalformedURLException.class, InterruptedException.class})
     public Object login(String tynamoLogin, String tynamoPassword, boolean tynamoRememberMe, String host) throws ShiroException
     {
         Subject currentUser = securityService.getSubject();
@@ -47,7 +49,17 @@ public class LoginFormBase
         token.setRememberMe(tynamoRememberMe);
         token.setHost(host);
 
-        currentUser.login(token);
+        try
+        {
+            currentUser.login(token);
+        } catch(AuthenticationException ae)
+        {
+            if(authDelayInterval > 0)
+            {
+                Thread.sleep(authDelayInterval * 1000);
+            }
+            throw ae;
+        }
 
         SavedRequest savedRequest = WebUtils.getAndClearSavedRequest(requestGlobals.getHTTPServletRequest());
 
@@ -90,6 +102,7 @@ public class LoginFormBase
     @Inject private PageService pageService;
     private @Inject PageRenderLinkSource linkSource;
     private @Inject @Symbol(SymbolConstants.SECURE_ENABLED) boolean isSecure;  
+    private @Inject @Symbol(SecurityModule.Symbols.INVALID_AUTH_DELAY) int authDelayInterval;
     private @Inject BaseURLSource urlSource;
 
 

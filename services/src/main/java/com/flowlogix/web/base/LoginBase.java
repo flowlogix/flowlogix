8e634e8c22030779cfc096304bb4b1c93256f7e0
lennyprimak <lennyprimak@localhost>
2011-10-08 22:53:35 -0400

    added deploy doc

---
+++ services/src/main/java/com/flowlogix/web/base/LoginBase.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flowlogix.web.base;
+
+import com.flowlogix.session.SessionTrackerHolder;
+import java.io.IOException;
+import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.annotations.BeginRender;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.ioc.annotations.Symbol;
+import org.apache.tapestry5.services.PageRenderLinkSource;
+import org.apache.tapestry5.services.RequestGlobals;
+
+/**
+ *
+ * @author lprimak
+ */
+public class LoginBase
+{        
+    /**
+     * when a user is redirected to the Logon page through a AJAX
+     * request after their session has expired,
+     * return an json response that redirects them to the logon page
+     * @return
+     * @throws IOException
+     */
+    @BeginRender
+    private Object checkForAjax() throws IOException
+    {
+        SessionTrackerHolder.redirectToSelf(rg, linkSource, isSecure);
+        return null;
+    }
+   
+    private @Inject RequestGlobals rg;
+    private @Inject PageRenderLinkSource linkSource;
+    private @Inject @Symbol(SymbolConstants.SECURE_ENABLED) boolean isSecure;  
+}
---
+++ services/src/main/java/com/flowlogix/web/mixins/SessionTracker.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flowlogix.web.mixins;
+
+import com.flowlogix.session.SessionTrackerBase;
+import com.flowlogix.session.SessionTrackerHolder;
+import javax.servlet.http.HttpSession;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.RequestGlobals;
+
+/**
+ *
+ * @author lprimak
+ */
+public class SessionTracker implements SessionTrackerBase
+{
+    @Override
+    public boolean isValidSession() 
+    {
+        return Boolean.TRUE.equals(hasSession);
+    }    
+    
+        
+    @SetupRender
+    private void setSession()
+    {
+        if (rg.getRequest().isXHR() == false)
+        {
+            HttpSession session = rg.getHTTPServletRequest().getSession(false);
+            hasSession = session != null;
+            if(hasSession == true)
+            {
+                holder.setPageSession(rg.getActivePageName(), session, this);
+            }
+        }
+    }
+
+    
+    private @Inject RequestGlobals rg;  
+    private @Persist Boolean hasSession;
+    private SessionTrackerHolder holder = SessionTrackerHolder.get();
+}

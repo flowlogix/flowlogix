30701a281e6698f1dbcb94462b30a00974d45f57
lprimak <lprimak@localhost>
2011-10-14 14:00:40 -0400

    added AJAX highlight color override support

---
+++ services/src/main/java/com/flowlogix/util/Streams.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flowlogix.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author lprimak
+ */
+public class Streams
+{
+    public static String readString(InputStream strm) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(strm));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+
+        while ((line = br.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+
+        return sb.toString();
+    }
+}
--- services/src/main/java/com/flowlogix/web/mixins/ColorHighlightOverride.java
+++ services/src/main/java/com/flowlogix/web/mixins/ColorHighlightOverride.java
@@ -1,23 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.flowlogix.web.mixins;
 
 import com.flowlogix.util.Streams;
 import java.io.IOException;
 import java.io.InputStream;
-import javax.validation.constraints.NotNull;
-
 import lombok.Cleanup;
 import org.apache.tapestry5.Asset;
-import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.annotations.BindParameter;
 import org.apache.tapestry5.annotations.Environmental;
-import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Path;
 import org.apache.tapestry5.annotations.SetupRender;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 
-public class ColorHighlight
+/**
+ *
+ * @author lprimak
+ */
+public class ColorHighlightOverride
 {
-    public ColorHighlight() throws IOException
+    public ColorHighlightOverride() throws IOException
     {
         @Cleanup InputStream strm = scriptAsset.getResource().openStream();
         script = Streams.readString(strm);
@@ -27,14 +32,13 @@ public class ColorHighlight
     @SetupRender
     void init() throws IOException
     {
-        js.addScript(script, color);
+        js.addScript(script, highlightColor);
     }
     
     
     @Environmental private JavaScriptSupport js;
-    @Parameter(required=true, defaultPrefix=BindingConstants.LITERAL) @NotNull 
-    private String color;
-    @Inject @Path("ColorHighlight.js")
+    private @BindParameter String highlightColor;
+    @Inject @Path("EffectOverrides.js")
     private Asset scriptAsset;
     private final String script;
 }

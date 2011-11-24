ebd2d067c2976204168cb4ee4670d4a4b7b04b7a
lprimak <lprimak@localhost>
2011-11-24 15:26:07 -0500

    manual error in dates patch - wirj ub origress

--- services/src/main/java/com/flowlogix/web/mixins/CalendarPopupPatch.java
+++ services/src/main/java/com/flowlogix/web/mixins/CalendarPopupPatch.java
@@ -4,7 +4,12 @@
  */
 package com.flowlogix.web.mixins;
 
+import com.flowlogix.util.Streams;
 import com.flowlogix.web.services.AssetMinimizer;
+import java.io.IOException;
+import java.io.InputStream;
+import lombok.Cleanup;
+import lombok.SneakyThrows;
 import org.apache.tapestry5.Asset;
 import org.apache.tapestry5.annotations.Environmental;
 import org.apache.tapestry5.annotations.Path;
@@ -22,12 +27,24 @@ public class CalendarPopupPatch
     @SetupRender
     void init()
     {
-        js.addScript(script);
+        js.addScript(calPopupScript);
+        js.addScript(dateFieldParserScript);
+    }
+
+    
+    @SneakyThrows(IOException.class)
+    public CalendarPopupPatch()
+    {
+        @Cleanup InputStream strm = dateFieldParserscriptAsset.getResource().openStream();
+        dateFieldParserScript = Streams.readString(strm);
     }
     
     
     private @Environmental JavaScriptSupport js;
-    private @Inject @Path("CalendarPopupPatch.js") Asset scriptAsset;
+    private @Inject @Path("CalendarPopupPatch.js") Asset calPopupScriptAsset;
+    private @Inject @Path("DateFieldParserPatch.js") Asset dateFieldParserscriptAsset;
     private @Inject AssetMinimizer minimizer;
-    private final String script = minimizer.minimize(scriptAsset);
+    private final String calPopupScript = minimizer.minimize(calPopupScriptAsset);
+    // +++ private final String dateFieldParserScript = minimizer.minimize(dateFieldParserscriptAsset);
+    private final String dateFieldParserScript;
 }

88fbe334b807a4b328a0e86c7417a10f3ea2f0c6
lprimak <lprimak@localhost>
2011-11-24 16:06:26 -0500

    date field is now parsing improper manual inputs properly, also reset to blank is handled correctly

--- services/src/main/java/com/flowlogix/web/mixins/CalendarPopupPatch.java
+++ services/src/main/java/com/flowlogix/web/mixins/CalendarPopupPatch.java
@@ -4,12 +4,7 @@
  */
 package com.flowlogix.web.mixins;
 
-import com.flowlogix.util.Streams;
 import com.flowlogix.web.services.AssetMinimizer;
-import java.io.IOException;
-import java.io.InputStream;
-import lombok.Cleanup;
-import lombok.SneakyThrows;
 import org.apache.tapestry5.Asset;
 import org.apache.tapestry5.annotations.Environmental;
 import org.apache.tapestry5.annotations.Path;
@@ -32,19 +27,10 @@ public class CalendarPopupPatch
     }
 
     
-    @SneakyThrows(IOException.class)
-    public CalendarPopupPatch()
-    {
-        @Cleanup InputStream strm = dateFieldParserscriptAsset.getResource().openStream();
-        dateFieldParserScript = Streams.readString(strm);
-    }
-    
-    
     private @Environmental JavaScriptSupport js;
     private @Inject @Path("CalendarPopupPatch.js") Asset calPopupScriptAsset;
     private @Inject @Path("DateFieldParserPatch.js") Asset dateFieldParserscriptAsset;
     private @Inject AssetMinimizer minimizer;
     private final String calPopupScript = minimizer.minimize(calPopupScriptAsset);
-    // +++ private final String dateFieldParserScript = minimizer.minimize(dateFieldParserscriptAsset);
-    private final String dateFieldParserScript;
+    private final String dateFieldParserScript = minimizer.minimize(dateFieldParserscriptAsset);
 }

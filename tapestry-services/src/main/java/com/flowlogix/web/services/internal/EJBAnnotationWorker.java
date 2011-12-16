25d7b99ed97fc3326795decaa1f6d26a9350fb94
lprimak <lprimak@localhost>
2011-12-16 16:21:42 -0500

    Issue 2- added support for Tapestry @EJB injection of @Stateful session beans

--- tapestry-services/src/main/java/com/flowlogix/web/services/internal/EJBAnnotationWorker.java
+++ tapestry-services/src/main/java/com/flowlogix/web/services/internal/EJBAnnotationWorker.java
@@ -7,14 +7,20 @@
 package com.flowlogix.web.services.internal;
 
 import com.flowlogix.ejb.JNDIObjectLocator;
+import com.flowlogix.web.services.annotations.Stateful;
 import java.util.regex.Pattern;
 import javax.ejb.EJB;
 import javax.naming.NamingException;
 
 import lombok.SneakyThrows;
+import org.apache.tapestry5.internal.services.ComponentClassCache;
+import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.model.MutableComponentModel;
+import org.apache.tapestry5.plastic.FieldConduit;
+import org.apache.tapestry5.plastic.InstanceContext;
 import org.apache.tapestry5.plastic.PlasticClass;
 import org.apache.tapestry5.plastic.PlasticField;
+import org.apache.tapestry5.services.ApplicationStateManager;
 import org.apache.tapestry5.services.transform.ComponentClassTransformWorker2;
 import org.apache.tapestry5.services.transform.TransformationSupport;
 
@@ -27,13 +33,14 @@ import org.apache.tapestry5.services.transform.TransformationSupport;
 public class EJBAnnotationWorker implements ComponentClassTransformWorker2
 {
     @Override
-    @SneakyThrows(NamingException.class)
+    @SneakyThrows({NamingException.class})
     public void transform(PlasticClass plasticClass,
             TransformationSupport support, MutableComponentModel model)
     {
         for (PlasticField field : plasticClass.getFieldsWithAnnotation(EJB.class))
         {
             final EJB annotation = field.getAnnotation(EJB.class);
+            boolean isStateful = field.hasAnnotation(Stateful.class);
             String fieldType = field.getTypeName();
             String lookupname = null;
 
@@ -59,11 +66,10 @@ public class EJBAnnotationWorker implements ComponentClassTransformWorker2
 
             lookupname = prependPortableName(lookupname);
 
-            Object injectionValue = locator.getJNDIObject(lookupname);
+            Object injectionValue = lookupBean(field, fieldType, lookupname, isStateful);
 
             if (injectionValue != null)
             {
-                field.inject(injectionValue);
                 field.claim(annotation);
             }
         }
@@ -94,6 +100,53 @@ public class EJBAnnotationWorker implements ComponentClassTransformWorker2
         return lookupname;
     }
     
+    
+    private Object lookupBean(PlasticField field, final String typeName, 
+            final String lookupname, final boolean isStateful) throws NamingException
+    {
+        if(isStateful)
+        {
+            field.setConduit(new FieldConduit<Object>() 
+            {
+                @Override
+                @SneakyThrows(NamingException.class)
+                public Object get(Object instance, InstanceContext context)
+                {
+                    if(asm.exists(type))
+                    {
+                        return asm.get(type);
+                    }
+                    else
+                    {
+                        Object bean = locator.getJNDIObject(lookupname, isStateful);
+                        asm.set((Class<Object>) type, bean);
+                        return bean;
+                    }
+                }
+
+                
+                @Override
+                public void set(Object instance, InstanceContext context, Object newValue)
+                {
+                    asm.set((Class<Object>) type, newValue);
+                }
+                
+                
+                final Class<?> type = classCache.forName(typeName);
+            });            
+            return true;
+        }
+        else
+        {
+            Object rv = locator.getJNDIObject(lookupname, isStateful);
+            if(rv != null)
+            {
+                field.inject(rv);
+            }
+            return rv;
+        }
+    }
+    
 
     private boolean isBlankOrNull(String s)
     {
@@ -102,6 +155,8 @@ public class EJBAnnotationWorker implements ComponentClassTransformWorker2
     
 
     private final JNDIObjectLocator locator = new JNDIObjectLocator();
+    private @Inject ApplicationStateManager asm;
+    private @Inject ComponentClassCache classCache;
     private static final String REMOTE = "REMOTE";
     private static final String LOCAL = "LOCAL";
     public static final Pattern StripLocalPattern = Pattern.compile(LOCAL + "|" + REMOTE, Pattern.CASE_INSENSITIVE);

367652bf284caa58e959e61c8ea358a34d5c188f
lprimak <lprimak@localhost>
2011-12-17 12:03:25 -0500

    Issue 2: Added SessionAttribute support for stateful session beans

--- tapestry-services/src/main/java/com/flowlogix/web/services/internal/EJBAnnotationWorker.java
+++ tapestry-services/src/main/java/com/flowlogix/web/services/internal/EJBAnnotationWorker.java
@@ -3,6 +3,7 @@
  *
  * Copyright 2009 by Movellas ApS
  * All rights reserved. 
+ * Enhanced by Lenny Primak
  */
 package com.flowlogix.web.services.internal;
 
@@ -21,6 +22,8 @@ import org.apache.tapestry5.plastic.InstanceContext;
 import org.apache.tapestry5.plastic.PlasticClass;
 import org.apache.tapestry5.plastic.PlasticField;
 import org.apache.tapestry5.services.ApplicationStateManager;
+import org.apache.tapestry5.services.RequestGlobals;
+import org.apache.tapestry5.services.Session;
 import org.apache.tapestry5.services.transform.ComponentClassTransformWorker2;
 import org.apache.tapestry5.services.transform.TransformationSupport;
 
@@ -40,33 +43,11 @@ public class EJBAnnotationWorker implements ComponentClassTransformWorker2
         for (PlasticField field : plasticClass.getFieldsWithAnnotation(EJB.class))
         {
             final EJB annotation = field.getAnnotation(EJB.class);
-            boolean isStateful = field.hasAnnotation(Stateful.class);
-            String fieldType = field.getTypeName();
-            String lookupname = null;
+            final Stateful stateful = field.getAnnotation(Stateful.class);
+            final String fieldType = field.getTypeName();
+            final String lookupname = getLookupName(annotation, fieldType);
 
-            //try lookup
-            if (!isBlankOrNull(annotation.lookup()))
-            {
-                lookupname = annotation.lookup();
-            } //try name
-            else if(!isBlankOrNull(annotation.name()))
-            {
-                lookupname = annotation.name();
-            }
-            else if(!isBlankOrNull(annotation.beanName()))
-            {
-                lookupname = annotation.beanName();
-            }
-
-            //use type
-            if (lookupname == null)
-            {
-                lookupname = guessByType(fieldType);
-            }
-
-            lookupname = prependPortableName(lookupname);
-
-            Object injectionValue = lookupBean(field, fieldType, lookupname, isStateful);
+            Object injectionValue = lookupBean(field, fieldType, lookupname, stateful);
 
             if (injectionValue != null)
             {
@@ -101,44 +82,57 @@ public class EJBAnnotationWorker implements ComponentClassTransformWorker2
     }
     
     
-    private Object lookupBean(PlasticField field, final String typeName, 
-            final String lookupname, final boolean isStateful) throws NamingException
+    private String getLookupName(EJB annotation, String fieldType)
     {
-        if(isStateful)
+        String lookupname = null;
+        //try lookup
+        if (!isBlankOrNull(annotation.lookup()))
         {
-            field.setConduit(new FieldConduit<Object>() 
+            lookupname = annotation.lookup();
+        } //try name
+        else
+        {
+            if (!isBlankOrNull(annotation.name()))
             {
-                @Override
-                @SneakyThrows(NamingException.class)
-                public Object get(Object instance, InstanceContext context)
+                lookupname = annotation.name();
+            } else
+            {
+                if (!isBlankOrNull(annotation.beanName()))
                 {
-                    if(asm.exists(type))
-                    {
-                        return asm.get(type);
-                    }
-                    else
-                    {
-                        Object bean = locator.getJNDIObject(lookupname, isStateful);
-                        asm.set((Class<Object>) type, bean);
-                        return bean;
-                    }
+                    lookupname = annotation.beanName();
                 }
+            }
+        }
 
-                
-                @Override
-                public void set(Object instance, InstanceContext context, Object newValue)
-                {
-                    asm.set((Class<Object>) type, newValue);
-                }
-                
-                
-                final Class<?> type = classCache.forName(typeName);
-            });            
+        //use type
+        if (lookupname == null)
+        {
+            lookupname = guessByType(fieldType);
+        }
+
+        lookupname = prependPortableName(lookupname);
+        return lookupname;
+    }
+    
+    
+    private Object lookupBean(final PlasticField field, final String typeName, 
+            final String lookupname, final Stateful stateful) throws NamingException
+    {
+        if(stateful != null)
+        {
+            if(stateful.isSessionAttribute())
+            {
+                field.setConduit(new SessionAttributeFieldConduit(lookupname, stateful, field, typeName));              
+            }
+            else
+            {
+                field.setConduit(new AppStateFieldConduit(lookupname, stateful, field, typeName));
+            }
             return true;
         }
         else
         {
-            Object rv = locator.getJNDIObject(lookupname, isStateful);
+            Object rv = locator.getJNDIObject(lookupname, false);
             if(rv != null)
             {
                 field.inject(rv);
@@ -153,10 +147,81 @@ public class EJBAnnotationWorker implements ComponentClassTransformWorker2
         return s == null || s.trim().equals("");
     }
     
+    
+    private class AppStateFieldConduit implements FieldConduit<Object>
+    {
+        public AppStateFieldConduit(String lookupname, Stateful stateful, PlasticField field, String typeName)
+        {
+            this.lookupname = lookupname;
+            this.stateful = stateful;
+            this.field = field;
+            this.typeName = typeName;
+            this.type = classCache.forName(typeName);
+        }
+
+        
+        @Override
+        @SneakyThrows(NamingException.class)
+        public Object get(Object instance, InstanceContext context)
+        {
+            if(asm.exists(type))
+            {
+                return asm.get(type);
+            }
+            else
+            {
+                Object bean = locator.getJNDIObject(lookupname, stateful != null);
+                asm.set((Class<Object>) type, bean);
+                return bean;
+            }
+        }
+
+        
+        @Override
+        @SneakyThrows(IllegalAccessException.class)
+        public void set(Object instance, InstanceContext context, Object newValue)
+        {
+            throw new IllegalAccessException(String.format("Field %s is Read Only", field.getName()));
+        }
+        
+        
+        protected final String lookupname;
+        protected final Stateful stateful;
+        protected final PlasticField field;
+        protected final String typeName;
+        protected final Class<?> type;
+    }
+    
+    
+    private class SessionAttributeFieldConduit extends AppStateFieldConduit
+    {
+        public SessionAttributeFieldConduit(String lookupname, Stateful stateful, PlasticField field, String typeName)
+        {
+            super(lookupname, stateful, field, typeName);
+        }
+
+        
+        @Override
+        @SneakyThrows(NamingException.class)
+        public Object get(Object instance, InstanceContext context)
+        {
+            Session session = rg.getRequest().getSession(true);
+            Object rv = session.getAttribute(field.getName());
+            if(rv == null)
+            {
+                rv = locator.getJNDIObject(lookupname, stateful != null);
+                session.setAttribute(field.getName(), rv);
+            }
+            return rv;
+        }
+    }
+    
 
     private final JNDIObjectLocator locator = new JNDIObjectLocator();
     private @Inject ApplicationStateManager asm;
     private @Inject ComponentClassCache classCache;
+    private @Inject RequestGlobals rg;
+    
     private static final String REMOTE = "REMOTE";
     private static final String LOCAL = "LOCAL";
     public static final Pattern StripLocalPattern = Pattern.compile(LOCAL + "|" + REMOTE, Pattern.CASE_INSENSITIVE);

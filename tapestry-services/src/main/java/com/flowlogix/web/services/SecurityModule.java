7befe20f93e09aa1874e353a94705c5de2b83f72
lprimak <lprimak@localhost>
2012-01-29 09:53:58 -0500

    Issue #13- Compatibility with the new Tynamo and Shiro

--- tapestry-services/src/main/java/com/flowlogix/web/services/SecurityModule.java
+++ tapestry-services/src/main/java/com/flowlogix/web/services/SecurityModule.java
@@ -6,14 +6,7 @@ package com.flowlogix.web.services;
 
 import com.flowlogix.web.services.internal.PageServiceOverride;
 import com.flowlogix.web.services.internal.SecurityInterceptorFilter;
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectStreamClass;
-import org.apache.shiro.io.DefaultSerializer;
-import org.apache.shiro.io.SerializationException;
-import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.WebSecurityManager;
 import org.apache.tapestry5.MetaDataConstants;
@@ -26,11 +19,7 @@ import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.ioc.annotations.Match;
 import org.apache.tapestry5.ioc.annotations.Symbol;
 import org.apache.tapestry5.ioc.services.ServiceOverride;
-import org.apache.tapestry5.services.ComponentRequestFilter;
-import org.apache.tapestry5.services.Request;
-import org.apache.tapestry5.services.RequestFilter;
-import org.apache.tapestry5.services.RequestHandler;
-import org.apache.tapestry5.services.Response;
+import org.apache.tapestry5.services.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tynamo.security.services.PageService;
@@ -121,52 +110,11 @@ public class SecurityModule
             else
             {
                 mgr.getCookie().setMaxAge(-1);
-            }
-            
-            // +++ Remove this once Shiro 1.2 is released and integrated
-            mgr.setSerializer(new Serialize<PrincipalCollection>());
+            }            
         }
         return null;
     }
-    
-    
-    /**
-     * @deprecated since Shiro 1.2
-     */
-    private static class Serialize<T> extends DefaultSerializer<T> 
-    {
-        @Override
-        public T deserialize(byte[] serialized) throws SerializationException
-        {
-            if (serialized == null)
-            {
-                String msg = "argument cannot be null.";
-                throw new IllegalArgumentException(msg);
-            }
-            ByteArrayInputStream bais = new ByteArrayInputStream(serialized);
-            BufferedInputStream bis = new BufferedInputStream(bais);
-            try
-            {
-                ObjectInputStream ois = new ObjectInputStream(bis)
-                {
-                    @Override
-                    public Class resolveClass(ObjectStreamClass desc) throws ClassNotFoundException
-                    {
-                        return Thread.currentThread().getContextClassLoader().loadClass(desc.getName());
-                    }
-                };
-                @SuppressWarnings({"unchecked"})
-                T deserialized = (T) ois.readObject();
-                ois.close();
-                return deserialized;
-            } catch (Exception e)
-            {
-                String msg = "Unable to deserialze argument byte array.";
-                throw new SerializationException(msg, e);
-            }
-        }
-    }    
-    
+        
     
     public static class Symbols
     {
--- tapestry-services/src/main/java/com/flowlogix/web/services/internal/PageServiceOverride.java
+++ tapestry-services/src/main/java/com/flowlogix/web/services/internal/PageServiceOverride.java
@@ -5,8 +5,12 @@
 package com.flowlogix.web.services.internal;
 
 import com.flowlogix.web.services.SecurityModule.Symbols;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.ioc.annotations.Symbol;
+import org.apache.tapestry5.services.LocalizationSetter;
 import org.tynamo.security.SecuritySymbols;
 import org.tynamo.security.services.PageService;
 import org.tynamo.security.services.impl.PageServiceImpl;
@@ -22,13 +26,16 @@ public class PageServiceOverride implements PageService
             @Inject @Symbol(Symbols.SUCCESS_URL) String successUrl,
             @Inject @Symbol(Symbols.LOGIN_URL) String loginUrl,
             @Inject @Symbol(Symbols.UNAUTHORIZED_URL) String unauthorizedUrl,
-            @Inject @Symbol(SecuritySymbols.UNAUTHORIZED_URL) String tynamoUnauthorizedUrl)
+            @Inject @Symbol(SecuritySymbols.UNAUTHORIZED_URL) String tynamoUnauthorizedUrl,
+            HttpServletRequest request, HttpServletResponse response,
+            LocalizationSetter localizationSetter)
     {
         if(unauthorizedUrl.isEmpty())
         {
             unauthorizedUrl = tynamoUnauthorizedUrl;
         }
-        impl = new PageServiceImpl(successUrl, loginUrl, unauthorizedUrl);
+        impl = new PageServiceImpl(successUrl, loginUrl, unauthorizedUrl,
+                request, response, localizationSetter);
     }
     
 
@@ -52,6 +59,34 @@ public class PageServiceOverride implements PageService
         return impl.getUnauthorizedPage();
     }
 
+    
+    @Override
+    public String getLocalelessPathWithinApplication()
+    {
+        return impl.getLocalelessPathWithinApplication();
+    }
+
+    
+    @Override
+    public String getLocaleFromPath(String path)
+    {
+        return impl.getLocaleFromPath(path);
+    }
+    
+
+    @Override
+    public void saveRequest()
+    {
+        impl.saveRequest();
+    }
+
+    
+    @Override
+    public void redirectToSavedRequest(String fallbackUrl) throws IOException
+    {
+        redirectToSavedRequest(fallbackUrl);
+    }
+    
     
     private final PageServiceImpl impl;
 }

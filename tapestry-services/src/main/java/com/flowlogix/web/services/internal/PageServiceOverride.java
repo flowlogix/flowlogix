dc7e15d099b4ef4d72b40ebf88db3f9b3bdaf623
lprimak <lprimak@localhost>
2012-03-20 21:51:10 -0400

    Issue 20: Work in Progress (probably invalid)Tapestry security 0.4.3

--- tapestry-services/src/main/java/com/flowlogix/web/services/internal/PageServiceOverride.java
+++ tapestry-services/src/main/java/com/flowlogix/web/services/internal/PageServiceOverride.java
@@ -6,8 +6,10 @@ package com.flowlogix.web.services.internal;
 
 import com.flowlogix.web.services.SecurityModule.Symbols;
 import java.io.IOException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.shiro.web.util.WebUtils;
 import org.apache.tapestry5.ioc.ObjectLocator;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.ioc.annotations.Symbol;
@@ -94,9 +96,69 @@ public class PageServiceOverride implements PageService
     @Override
     public void redirectToSavedRequest(String fallbackUrl) throws IOException
     {
-        init();
-        impl.redirectToSavedRequest(fallbackUrl);
+        init();     
+        /**
+         * See https://jira.codehaus.org/browse/TYNAMO-144
+         * Replace when fixed
+         */
+        _redirectToSavedRequest(fallbackUrl);
+      // impl.redirectToSavedRequest(fallbackUrl);
+  } 
+    
+    
+    /**
+     * See https://jira.codehaus.org/browse/TYNAMO-144 
+     * Remove when its fixed
+     * @param fallbackURL
+     * @throws IOException 
+     */
+    private void _redirectToSavedRequest(String fallbackURL) throws IOException
+    {
+        Cookie[] cookies = request.getCookies();
+        String requestUri = null;
+        if (cookies != null)
+        {
+            for (Cookie cookie : cookies)
+            {
+                if (WebUtils.SAVED_REQUEST_KEY.equals(cookie.getName()))
+                {
+                    requestUri = cookie.getValue();
+                    Cookie deleteCookie = createSavedRequestCookie();
+                    deleteCookie.setMaxAge(0);
+                    response.addCookie(deleteCookie);
+                    break;
+                }
+            }
+        }
+        if (requestUri == null)
+        {
+            requestUri = fallbackURL;
+        }
+        WebUtils.issueRedirect(request, response, requestUri, null, true, true);
     }
+    
+    
+    /**
+     * See https://jira.codehaus.org/browse/TYNAMO-144
+     * Remove when its fixed
+     */
+    private Cookie createSavedRequestCookie()
+    {
+        String requestUri = request.getRequestURI();
+        if (request.getQueryString() != null)
+        {
+            requestUri += "?" + request.getQueryString();
+        }
+        Cookie cookie = new Cookie(WebUtils.SAVED_REQUEST_KEY, requestUri);
+        String contextPath = request.getContextPath();
+        if ("".equals(contextPath))
+        {
+            contextPath = "/";
+        }
+        cookie.setPath(contextPath);
+        return cookie;
+    }
+
     
     
     private PageServiceImpl impl;

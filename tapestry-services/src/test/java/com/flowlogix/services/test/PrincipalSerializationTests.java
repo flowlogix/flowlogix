f3e7cb7c64baa842cf857ceeaccb4b3a5398cf5d
lprimak <lprimak@localhost>
2012-11-07 16:19:58 -0500

    Fixed bugs and made remote EJB authentication work

---
+++ tapestry-services/src/test/java/com/flowlogix/services/test/PrincipalSerializationTests.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2012 lprimak.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.flowlogix.services.test;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import org.apache.shiro.codec.Base64;
+import org.apache.shiro.io.DefaultSerializer;
+import org.apache.shiro.io.Serializer;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author lprimak
+ */
+public class PrincipalSerializationTests
+{
+    @Test
+    public void serializer()
+    {
+        final Serializer<X> ser = new DefaultSerializer<>();
+        final String encoded = Base64.encodeToString(ser.serialize(new X(15, "xxx")));
+        assertEquals(new X(15, "xxx"), ser.deserialize(Base64.decode(encoded)));
+    }
+
+    
+    @EqualsAndHashCode
+    @AllArgsConstructor
+    static class X implements Serializable
+    {
+        final Integer x;;
+        final String y;    
+    }
+}

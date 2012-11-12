0bf1ba5e4670ac8e13913b38d65f8b4e12415dcb
lprimak <lprimak@localhost>
2012-11-12 13:09:46 -0500

    refactored remote EJB access, made no-caching default for mappedName-configured remote EJB access

--- tapestry-services/src/main/java/com/flowlogix/ejb/JNDIConfigurer.java
+++ tapestry-services/src/main/java/com/flowlogix/ejb/JNDIConfigurer.java
@@ -17,21 +17,78 @@ package com.flowlogix.ejb;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.util.StringUtils;
 
 /**
  *
  * @author lprimak
  */
+@Slf4j
 public class JNDIConfigurer
 {
     private final Map<String, Config> _conf = new HashMap<>();
     private @Getter final Map<String, Config> configuration = Collections.synchronizedMap(_conf);
     private @Getter static JNDIConfigurer instance = new JNDIConfigurer();
+    
+    
+    public JNDIObjectLocator buildLocator(String mappedName) throws NamingException
+    {
+        if(StringUtils.hasText(mappedName) == false)
+        {
+            return new JNDIObjectLocator();
+        }
+        
+        Config config = getConfiguration().get(mappedName);
+        if(config == null)
+        {
+            log.error("Building EJB Locator Failed: mappedName %s is not configured", mappedName);
+            return new JNDIObjectLocator();
+        }
+        
+        Hashtable<String, String> env = new Hashtable<>();
+        if(StringUtils.hasText(config.getHostname()))
+        {
+            env.put("org.omg.CORBA.ORBInitialHost", config.getHostname());           
+        }
+        if(config.getPort() != null)
+        {
+            env.put("org.omg.CORBA.ORBInitialPort", config.getPort().toString());           
+        }
+        for(Map.Entry<String, String> entry : config.getAdditionalProperties().entrySet())
+        {
+            env.put(entry.getKey(), entry.getValue());
+        }
+        JNDIObjectLocator locator;
+        if(env.isEmpty())
+        {
+            locator = new JNDIObjectLocator();
+        }
+        else
+        {
+            locator = new JNDIObjectLocator(new InitialContext(env));
+        }
+        if(StringUtils.hasText(config.getPrefix()))
+        {
+            locator.setPortableNamePrefix(config.getPrefix());
+        }
+        locator.setNoCaching(!config.isCache());
+        return locator;
+    }
+    
+    
+    public JNDIObjectLocator buildLocator() throws NamingException
+    {
+        return buildLocator(null);
+    }
 
     
     @Data
@@ -39,9 +96,16 @@ public class JNDIConfigurer
     @NoArgsConstructor
     static public class Config
     {
+        public Config(String hostname, Integer port, String prefix)
+        {
+            this(hostname, port, prefix, false);
+        }
+        
+        
         String hostname;
         Integer port;
         String prefix;
+        boolean cache;
         final Map<String, String> additionalProperties = new HashMap<>();
     }
 }

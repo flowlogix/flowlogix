1b0cab88bf5bd891ee8035c7d3f3b4ce1029fda9
lprimak <lprimak@localhost>
2013-07-14 18:40:29 -0400

    Issue 43 - updated to more recent Tapestry JS

--- tapestry-services/src/main/resources/com/flowlogix/web/mixins/ColorHighlight.js
+++ tapestry-services/src/main/resources/com/flowlogix/web/mixins/ColorHighlight.js
@@ -2,11 +2,17 @@
  * Highlight zone updates in different colors
  */
 
-Tapestry.ElementEffect.colorhighlight = function(element) 
+Tapestry.ElementEffect.colorhighlight = function(element, color) 
 {
+    if (color)
+        return new Effect.Highlight(element, {
+            startcolor : '%s',
+            endcolor: color,
+            restorecolor: color
+        });
     return new Effect.Highlight(element, { 
         startcolor : '%s',
         restorecolor: $(element).savedBackgroundColor
-    }
+        }
     );
 };

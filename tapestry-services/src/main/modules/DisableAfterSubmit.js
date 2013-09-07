a7f5193fe4c6d70b95558c7988a1b3728e5d97f9
lprimak <lprimak@localhost>
2013-09-07 04:02:24 -0400

    Issue 41 - forgot to add and delete couple of files

---
+++ tapestry-services/src/main/modules/DisableAfterSubmit.js
@@ -0,0 +1,68 @@
+/* 
+ * Copyright 2013 lprimak.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* 
+ * Disable Buttons after Submit
+ */
+
+(function() {
+  define(["t5/core/dom", "t5/core/events", "underscore", "t5/core/zone"], 
+  function(dom, events, _, zone) {
+      var metaButtonIdStr = 'disabled-after-submit-id';
+      var metaButtonExcluded = 'disable-after-submit-excluded';
+      
+      var enableButtons = function(self) {
+            var btnElt = self.meta(metaButtonIdStr);
+            if (btnElt != null) {
+                btnElt.attribute('disabled', null);
+                self.meta(metaButtonIdStr, null);
+            }
+      };
+      
+      var findZone = function(elt) {
+          var formElt = elt.findParent('form');
+          var zoneElt = (formElt !== null)? zone.findZone(formElt) : null; 
+          return zoneElt;
+      };
+      
+      return function() {
+          dom.onDocument('click', 'input[type=submit], input[type=button]', function() { 
+              if(this.meta(metaButtonExcluded) == true) { return; }
+              var zoneElt = findZone(this);
+              if(zoneElt !== null) {
+                  enableButtons(zoneElt);
+                  zoneElt.meta(metaButtonIdStr, this);
+              }
+              _.defer(function(self) {
+                  if(zoneElt.meta(metaButtonIdStr) !== null) {
+                     self.attribute('disabled', 'disabled');                   
+                  }
+              }, this);
+          });
+          
+          dom.onDocument(events.zone.didUpdate, function() {
+              enableButtons(this);
+          });
+          
+          dom.onDocument(events.field.showValidationError, function() {
+              var zoneElt = findZone(this);
+              if(zoneElt != null) {
+                enableButtons(zoneElt);
+              }
+          });
+      };
+  });
+}).call(this);

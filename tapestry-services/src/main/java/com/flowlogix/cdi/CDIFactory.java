b49f3fea3bd05f9eca4fc1195b4e5870e65fbef8
lprimak <lprimak@localhost>
2014-06-05 20:03:36 -0400

    Issue 61 refactoring

--- tapestry-services/src/main/java/com/flowlogix/cdi/CDIFactory.java
+++ tapestry-services/src/main/java/com/flowlogix/cdi/CDIFactory.java
@@ -1,8 +1,7 @@
 /**
  * @(#)CDIFactory.java
  *
- * Copyright 2009 by Movellas ApS
- * All rights reserved. 
+ * Copyright 2009 by Movellas ApS All rights reserved.
  */
 package com.flowlogix.cdi;
 
@@ -10,43 +9,45 @@ import java.util.Set;
 import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 
 /**
- * 
+ *
  * @author Magnus
  */
-public class CDIFactory {
-
-	private final BeanManager beanManager;
-	private final Logger log;
-	public CDIFactory(Logger log, BeanManager bm) {
-		this.beanManager = bm;
-		this.log = log;
-	}
-	
-	public <T> T get(Class<T> clazz) {
-            Set<Bean<?>> beans;
+@RequiredArgsConstructor
+public class CDIFactory
+{
+    public <T> T get(Class<T> clazz)
+    {
+        Set<Bean<?>> beans;
+        try
+        {
+            beans = beanManager.getBeans(clazz);
+        } catch (UnsupportedOperationException e)
+        {
+            return null;
+        }
+        if (beans != null && beans.size() > 0)
+        {
+            @SuppressWarnings("unchecked")
+            Bean<T> bean = (Bean<T>) beans.iterator().next();
+            CreationalContext<T> ctx = beanManager.createCreationalContext(bean);
             try
             {
-                beans =  beanManager.getBeans(clazz);
-            }
-            catch(UnsupportedOperationException e)
+                T o = clazz.cast(beanManager.getReference(bean, clazz, ctx));
+                log.debug("Found and returning: " + clazz.getCanonicalName());
+                return o;
+            } catch (Exception e)
             {
-                return null;
+                log.trace("CDI Retrieval Error", e);
             }
-		if(beans!=null && beans.size()>0) {
-                        @SuppressWarnings("unchecked")
-			Bean<T> bean = (Bean<T>) beans.iterator().next();		
-			CreationalContext<T> ctx = beanManager.createCreationalContext(bean);	
-                        try
-                        {
-                            T o = clazz.cast(beanManager.getReference(bean, clazz, ctx)); 
-                            log.debug("Found and returning: "+clazz.getCanonicalName());
-                            return o;
-                        }
-                        catch(Exception e) {}
-		}
-		return null;
-	}
+        }
+        return null;
+    }
+
+    
+    private final Logger log;
+    private final BeanManager beanManager;
 }

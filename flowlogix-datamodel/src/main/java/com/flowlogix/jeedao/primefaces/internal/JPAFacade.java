b21c156f7351262aa36518af3addcef8cceb850b
lprimak <lprimak@localhost>
2014-12-08 12:19:12 -0500

    cleanup and license headers

--- flowlogix-datamodel/src/main/java/com/flowlogix/jeedao/primefaces/internal/JPAFacade.java
+++ flowlogix-datamodel/src/main/java/com/flowlogix/jeedao/primefaces/internal/JPAFacade.java
@@ -1,19 +1,27 @@
 /*
- * Copyright (C) 2001-2014, Bett-A-Way, Inc.
- * All rights reserved.
+ * Copyright 2014 lprimak.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are prohibited unless authorized in writing by Bett-A-Way, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.flowlogix.jeedao.primefaces.internal;
 
+import com.flowlogix.jeedao.impl.AbstractFacade;
 import com.flowlogix.jeedao.primefaces.JPALazyDataModel;
 import com.flowlogix.jeedao.primefaces.JPALazyDataModel.EntityManagerGetter;
 import com.flowlogix.jeedao.primefaces.JPALazyDataModel.Filter;
 import com.flowlogix.jeedao.primefaces.JPALazyDataModel.Filter.FilterData;
 import com.flowlogix.jeedao.primefaces.JPALazyDataModel.Optimizer;
 import com.flowlogix.jeedao.primefaces.JPALazyDataModel.Sorter;
-import com.flowlogix.jeedao.impl.AbstractFacade;
 import com.flowlogix.util.TypeConverter;
 import com.google.common.base.Optional;
 import com.google.common.collect.FluentIterable;

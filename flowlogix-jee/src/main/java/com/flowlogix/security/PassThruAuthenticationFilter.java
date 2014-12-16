e50de1cda5e3dde716cfe49a7a148504e9953d93
lprimak <lprimak@localhost>
2014-12-15 20:01:26 -0500

    added PathUtil, and Shiro filters

---
+++ flowlogix-jee/src/main/java/com/flowlogix/security/PassThruAuthenticationFilter.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2014 lprimak.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.flowlogix.security;
+
+import com.flowlogix.ui.AttributeKeys;
+import com.flowlogix.util.PathUtil;
+import com.google.common.base.Optional;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterators;
+import com.google.common.net.HttpHeaders;
+import java.io.IOException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.HttpMethod;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.web.util.WebUtils;
+import org.omnifaces.util.Servlets;
+
+/**
+ * Implements JSF Ajax redirection via OmniFaces
+ * 
+ * @author lprimak
+ */
+@Slf4j
+public class PassThruAuthenticationFilter extends org.apache.shiro.web.filter.authc.PassThruAuthenticationFilter
+{
+    @Override
+    protected void redirectToLogin(ServletRequest request, ServletResponse response) throws IOException
+    {
+        boolean isGetRequest = HttpMethod.GET.equalsIgnoreCase(WebUtils.toHttp(request).getMethod());
+        String sessionExpired = Iterators.getLast(Splitter.on('.')
+                .split(AttributeKeys.SESSION_EXPIRED_KEY).iterator());
+        Servlets.facesRedirect(WebUtils.toHttp(request), WebUtils.toHttp(response),
+                Servlets.getRequestBaseURL(WebUtils.toHttp(request))
+                + getLoginUrl().replaceFirst("^/", "") + (isGetRequest? "" : "?%s=true"), 
+                sessionExpired);
+    }
+
+    
+    @Override
+    protected boolean isLoginRequest(ServletRequest request, ServletResponse response)
+    {
+        boolean rv = super.isLoginRequest(request, response);
+        if(rv && HttpMethod.GET.equalsIgnoreCase(WebUtils.toHttp(request).getMethod()))
+        {
+            saveRequest(request, response, true);
+        }
+        return rv;
+    }
+
+    
+    @Override
+    protected void saveRequestAndRedirectToLogin(ServletRequest request, ServletResponse response) throws IOException
+    {
+        saveRequest(request, response, false);
+        redirectToLogin(request, response);
+    }
+
+     
+    private void saveRequest(ServletRequest request, ServletResponse response, boolean useReferer)
+    {
+        Optional<String> path = useReferer? getReferer(WebUtils.toHttp(request)) 
+                : Optional.of(Servlets.getRequestURLWithQueryString(WebUtils.toHttp(request)));
+        if(!path.isPresent())
+        {
+            return;
+        }
+        
+        Servlets.addResponseCookie(WebUtils.toHttp(request), WebUtils.toHttp(response), 
+                WebUtils.SAVED_REQUEST_KEY, path.get(), null,
+                PathUtil.getContextPath(WebUtils.toHttp(request)), -1);
+    }    
+    
+    
+    private static Optional<String> getReferer(HttpServletRequest request)
+    {
+        String referer = request.getHeader(HttpHeaders.REFERER);
+        if (referer != null)
+        {
+            // do not switch to https if custom port is specified
+            if(!referer.matches("^http:\\/\\/[A-z|.|[0-9]]+:[0-9]+\\/.*"))
+            {
+                referer = referer.replaceFirst("^http:", "https:");
+            }
+        }
+
+        return Optional.fromNullable(referer);
+    }    
+    
+
+    @Override
+    protected void saveRequest(ServletRequest request)
+    {
+        throw new UnsupportedOperationException("bad op");
+    }
+}
--- flowlogix-jee/src/main/java/com/flowlogix/util/PathUtil.java
+++ flowlogix-jee/src/main/java/com/flowlogix/util/PathUtil.java
@@ -15,11 +15,77 @@
  */
 package com.flowlogix.util;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
+import org.apache.commons.lang.StringUtils;
+import org.apache.shiro.web.util.WebUtils;
+
 /**
- *
+ * builds absolute path URI when needed for redirection from Web and REST API
+ * for web pages
+ * 
  * @author lprimak
  */
 public class PathUtil
 {
+    /**
+     * Returns full path URI for a relative path
+     * 
+     * @param request
+     * @param relativePath
+     * @return relative URI
+     */
+    @SneakyThrows(URISyntaxException.class)
+    public static URI toAppURI(HttpServletRequest request, String relativePath)
+    {
+        String absolutePath = String.format("%s%s/%s", getServerPath(request),
+                request.getContextPath(), relativePath);
+
+        return new URI(absolutePath);
+    }
     
+    
+    /**
+     * @param request
+     * @return complete server path i.e. https://www.glowlogix.com:8080
+     */
+    public static String getServerPath(HttpServletRequest request)
+    {
+        String port = "";
+        switch(request.getScheme())
+        {
+            case "http":
+                if(request.getServerPort() != 80)
+                {
+                    port = ":" + Integer.toString(request.getServerPort());
+                }
+                break;
+            case "https":
+                if(request.getServerPort() != 443)
+                {
+                    port = ":" + Integer.toString(request.getServerPort());
+                }
+                break;
+        }
+        
+        return String.format("%s://%s%s", 
+                request.getScheme(), request.getServerName(), port);
+    }
+    
+    
+    /**
+     * @param request
+     * @return context path or "/" if there is no context path
+     */
+    public static String getContextPath(HttpServletRequest request)
+    {
+        String contextPath = WebUtils.toHttp(request).getContextPath();
+        if(StringUtils.isBlank(contextPath))
+        {
+            contextPath = "/";
+        }
+        return contextPath;
+    }
 }

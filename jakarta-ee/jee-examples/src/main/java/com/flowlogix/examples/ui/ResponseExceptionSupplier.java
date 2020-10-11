cad2c52649f21cd53fc79b5b8c1e0b9aa44753a5
lprimak <lenny@flowlogix.com>
2020-10-10 21:32:36 -0500

    able to test exception handling properly

---
+++ jakarta-ee/jee-examples/src/main/java/com/flowlogix/examples/ui/ResponseExceptionSupplier.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 lprimak.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.flowlogix.examples.ui;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+import org.omnifaces.util.Faces;
+
+/**
+ *
+ * @author lprimak
+ */
+public class ResponseExceptionSupplier implements PhaseListener {
+    static final String RUN_BEFORE_RESPONSE = "com.flowlogix.response.runBefore";
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void afterPhase(PhaseEvent event) {
+    }
+
+    @Override
+    public void beforePhase(PhaseEvent event) {
+        Runnable before = Faces.getRequestAttribute(RUN_BEFORE_RESPONSE);
+        if (before != null) {
+            before.run();
+        }
+    }
+
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.RENDER_RESPONSE;
+    }
+}

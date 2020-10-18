9c5c9b9e0c7396b18dc6a4d904fdd073cff0a6ff
lprimak <lenny@flowlogix.com>
2020-10-18 16:52:27 -0500

    added more testing and production mode

--- jakarta-ee/jee-examples/src/test/java/com/flowlogix/examples/ExceptionPageTest.java
+++ jakarta-ee/jee-examples/src/test/java/com/flowlogix/examples/ExceptionPageTest.java
@@ -16,6 +16,8 @@
 package com.flowlogix.examples;
 
 import com.flowlogix.test.UserInterfaceTest;
+import java.util.List;
+import org.codehaus.plexus.util.StringUtils;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import static org.jboss.arquillian.graphene.Graphene.guardAjax;
@@ -25,9 +27,11 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.archive.importer.MavenImporter;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -63,6 +67,12 @@ public class ExceptionPageTest {
     @FindBy(id = "form:lateSqlThrow")
     private WebElement lateSqlThrow;
 
+    @FindBy(id = "end-of-page")
+    private WebElement endOfPage;
+
+    @FindBy(id = "mode")
+    private WebElement modeField;
+
     private static final String APP_NAME = "exctest";
     private static final String URL = "http://localhost:8080/";
 
@@ -94,6 +104,40 @@ public class ExceptionPageTest {
         guardAjax(lateSqlThrow).click();
         assertEquals("Exception happened", exceptionHeading.getText());
         assertEquals("Exception type: class java.sql.SQLException", exceptionTypeField.getText());
+    }
+
+    @Test
+    public void versions() {
+        webDriver.get(URL + APP_NAME + "/exception-pages.xhtml");
+        waitGui(webDriver);
+        String expected = "end of page";
+        if ("Production".equals(modeField.getText())) {
+            expected = "end of page - minimized";
+        }
+        assertEquals(expected, endOfPage.getText());
+        List<WebElement> scripts = webDriver.findElements(By.tagName("script"));
+        int count = 0;
+        for(WebElement script : scripts) {
+            String href = script.getAttribute("src");
+            if (StringUtils.isBlank(href)) {
+                continue;
+            }
+            assertTrue("not versioned", href.contains("v="));
+            ++count;
+        }
+        assertEquals(3, count);
+
+        count = 0;
+        List<WebElement> csses = webDriver.findElements(By.tagName("link"));
+        for(WebElement css : csses) {
+            String href = css.getAttribute("href");
+            if (StringUtils.isBlank(href)) {
+                continue;
+            }
+            assertTrue("not versioned", href.contains("v="));
+            ++count;
+        }
+        assertEquals(1, count);
     }
 
     @Deployment(testable = false)

48cfac62373fd08feb4db3228ea2b6ac4736b8eb
lprimak <lenny@flowlogix.com>
2020-10-20 20:59:59 -0500

    added user viewer

---
+++ jakarta-ee/jee-examples/src/main/java/com/flowlogix/examples/data/Initializer.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 lprimak.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.flowlogix.examples.data;
+
+import com.flowlogix.examples.entities.UserEntity;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author lprimak
+ */
+@Singleton @Startup
+public class Initializer {
+    @PersistenceContext
+    private EntityManager em;
+
+    @PostConstruct
+    void init() {
+        List<UserEntity> userList = Stream.of(
+                UserEntity.builder().userId("lprimak").fullName("Lenny Primak")
+                .address("3000 Summit Blvd").zipCode(68502).build(),
+                UserEntity.builder().userId("jprimak").fullName("Jill Primak")
+                .address("3000 Summit Blvd").zipCode(68502).build(),
+                UserEntity.builder().userId("anya").fullName("Anya Primak")
+                .address("3000 Summit Blvd").zipCode(68502).build(),
+                UserEntity.builder().userId("gene").fullName("Gene Volovich")
+                .address("NY, Somewhere").zipCode(10012).build(),
+                UserEntity.builder().userId("lilah").fullName("Lilah Keiser")
+                .address("5151 Bison Drive").zipCode(68502).build()
+        ).collect(Collectors.toList());
+        userList.forEach(em::merge);
+    }
+}
---
+++ jakarta-ee/jee-examples/src/main/java/com/flowlogix/examples/entities/UserEntity.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 lprimak.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.flowlogix.examples.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author lprimak
+ */
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String userId;
+    private String fullName;
+    private String address;
+    private Integer zipCode;
+}

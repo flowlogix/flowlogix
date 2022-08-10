982ba906aef120c3c8e1e3173f850deddca198e0
lprimak <lenny@flowlogix.com>
2022-08-09 23:57:26 -0500

    working login page

---
+++ jakarta-ee/jee-examples/src/main/java/com/flowlogix/examples/shiro/LoginBean.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 lprimak.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.flowlogix.examples.shiro;
+
+import javax.enterprise.inject.Model;
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.web.util.WebUtils;
+import org.omnifaces.util.Faces;
+
+/**
+ *
+ * @author lprimak
+ */
+@Model
+@Getter @Setter
+@Slf4j
+public class LoginBean {
+    @NotBlank
+    private String uname;
+    @NotBlank
+    private String pwd;
+
+    public void login() {
+        SecurityUtils.getSubject().login(new UsernamePasswordToken(uname, pwd, false));
+        redirect();
+    }
+
+    public void logout() {
+        SecurityUtils.getSubject().logout();
+        redirect();
+    }
+
+    void redirect() {
+        String savedRequest = Faces.getRequestCookie(WebUtils.SAVED_REQUEST_KEY);
+        if (savedRequest != null) {
+            Faces.redirect(savedRequest);
+        } else {
+            Faces.redirect(Faces.getRequestContextPath() + Faces.getViewId());
+        }
+    }
+}

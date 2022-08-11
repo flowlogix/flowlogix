194a9973183e6954f09e978c874786c88eab2cd8
lprimak <lenny@flowlogix.com>
2022-08-10 22:11:43 -0500

    added a simple form to example

---
+++ jakarta-ee/jee-examples/src/main/java/com/flowlogix/examples/shiro/FormBean.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022 lprimak.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.flowlogix.examples.shiro;
+
+import javax.enterprise.inject.Model;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.omnifaces.util.Faces;
+
+/**
+ *
+ * @author lprimak
+ */
+@Model
+@Getter @Setter
+@Slf4j
+public class FormBean {
+    private String firstName;
+    private String lastName;
+
+    public void submit() {
+        log.info("Form Submitted - firstName: {}, lastName: {}", firstName, lastName);
+        Faces.refresh();
+    }
+}

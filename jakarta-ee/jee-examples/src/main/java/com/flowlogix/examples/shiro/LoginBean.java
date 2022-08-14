c9bdf392cc3dfdbe8977a2f2c5b71d3ac3ae529a
lprimak <lenny@flowlogix.com>
2022-08-14 06:00:24 -0500

    support stateful JSF bean form submit

--- jakarta-ee/jee-examples/src/main/java/com/flowlogix/examples/shiro/LoginBean.java
+++ jakarta-ee/jee-examples/src/main/java/com/flowlogix/examples/shiro/LoginBean.java
@@ -21,9 +21,12 @@ import java.net.CookieManager;
 import java.net.HttpCookie;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Pattern;
 import javax.enterprise.inject.Model;
 import javax.servlet.http.Cookie;
 import javax.validation.constraints.NotBlank;
@@ -34,7 +37,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import static org.apache.shiro.web.servlet.ShiroHttpSession.DEFAULT_SESSION_ID_NAME;
 import org.apache.shiro.web.util.WebUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
 import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
 import org.omnifaces.util.Servlets;
@@ -52,6 +58,11 @@ public class LoginBean {
     @NotBlank
     private String pwd;
     private boolean rememberMe;
+
+    // encoded view state
+    static final Pattern VIEW_STATE_PATTERN = Pattern.compile("javax.faces.ViewState=([%\\d\\w:-]+)");
+    static final String FACES_VIEW_STATE = "javax.faces.ViewState";
+    static final String FACES_VIEW_STATE_EQUALS = FACES_VIEW_STATE + "=";
 
     public void login() {
         try {
@@ -79,7 +90,7 @@ public class LoginBean {
 
             String savedFormData = Faces.getRequestCookie(SHIRO_FORM_DATA);
             if (savedFormData != null) {
-                log.debug("form data: {}", savedFormData);
+                log.debug("saved form data: {}", savedFormData);
                 var formDataCookieToDelete = new Cookie(SHIRO_FORM_DATA, "tbd");
                 formDataCookieToDelete.setPath(Servlets.getContext().getContextPath());
                 formDataCookieToDelete.setMaxAge(0);
@@ -87,24 +98,49 @@ public class LoginBean {
 
                 CookieManager cookieManager = new CookieManager();
                 HttpCookie cookie = new HttpCookie(Servlets.getContext().getSessionCookieConfig().getName() != null
-                        ? Servlets.getContext().getSessionCookieConfig().getName() : "JSESSIONID",
+                        ? Servlets.getContext().getSessionCookieConfig().getName() : DEFAULT_SESSION_ID_NAME,
                         SecurityUtils.getSubject().getSession().getId().toString());
                 cookie.setPath(Servlets.getContext().getContextPath());
                 cookieManager.getCookieStore().add(new URI(savedRequest), cookie);
                 HttpClient client = HttpClient.newBuilder().cookieHandler(cookieManager)
                         .build();
-                HttpRequest request = HttpRequest.newBuilder()
+                HttpRequest getRequest = HttpRequest.newBuilder()
+                        .uri(URI.create(savedRequest))
+                        .GET().build();
+                HttpResponse<String> htmlResponse = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
+                if (htmlResponse.statusCode() != 200) {
+                    Faces.redirect(savedRequest);
+                    return;
+                }
+                Elements elts = Jsoup.parse(htmlResponse.body()).select(String.format("input[name=%s]", FACES_VIEW_STATE));
+                if (!elts.isEmpty()) {
+                    String viewState = elts.first().attr("value");
+
+                    var matcher = VIEW_STATE_PATTERN.matcher(savedFormData);
+                    if (matcher.find()) {
+                         savedFormData = matcher.replaceFirst(FACES_VIEW_STATE_EQUALS +
+                                URLEncoder.encode(viewState, StandardCharsets.UTF_8));
+                        log.debug("Encoded w/Replaced ViewState: {}", savedFormData);
+                    }
+                }
+                HttpRequest postRequest = HttpRequest.newBuilder()
                         .uri(URI.create(savedRequest))
                         .POST(HttpRequest.BodyPublishers.ofString(savedFormData))
                         .headers("Content-Type", "application/x-www-form-urlencoded")
                         .build();
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                log.debug("requeust: {}, response: {}", request, response);
-                if (response.statusCode() == 302) {
-                    Faces.redirect(response.headers().firstValue("Location").orElseThrow());
-                } else {
-                    Faces.getResponse().getWriter().append(response.body());
-                    Faces.responseComplete();
+                HttpResponse<String> response = client.send(postRequest, HttpResponse.BodyHandlers.ofString());
+                log.debug("requeust: {}, response: {}", postRequest, response);
+                switch (response.statusCode()) {
+                    case 302:
+                        Faces.redirect(response.headers().firstValue("Location").orElseThrow());
+                        break;
+                    case 200:
+                        Faces.getResponse().getWriter().append(response.body());
+                        Faces.responseComplete();
+                        break;
+                    default:
+                        Faces.redirect(savedRequest);
+                        break;
                 }
             } else {
                 Faces.redirect(savedRequest);

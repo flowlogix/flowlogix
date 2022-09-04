a2cd4b03de668ac99e998eb1ac9ce4cd5d1a9ee8
lprimak <lenny@flowlogix.com>
2022-09-04 13:28:20 -0700

    catch exception when initialization fails

--- jakarta-ee/shiro-ee/src/main/java/com/flowlogix/shiro/ee/listeners/IniEnvironment.java
+++ jakarta-ee/shiro-ee/src/main/java/com/flowlogix/shiro/ee/listeners/IniEnvironment.java
@@ -20,6 +20,7 @@ import com.flowlogix.shiro.ee.filters.LogoutFilter;
 import com.flowlogix.shiro.ee.filters.SslFilter;
 import java.util.Map;
 import javax.servlet.Filter;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.config.Ini;
 import org.apache.shiro.web.config.WebIniSecurityManagerFactory;
 import org.apache.shiro.web.env.IniWebEnvironment;
@@ -30,6 +31,7 @@ import org.apache.shiro.web.filter.mgt.DefaultFilter;
  *
  * @author lprimak
  */
+@Slf4j
 public class IniEnvironment extends IniWebEnvironment {
     private String otherConfigLocation;
 
@@ -39,9 +41,15 @@ public class IniEnvironment extends IniWebEnvironment {
         protected Map<String, ?> createDefaults(Ini ini, Ini.Section mainSection) {
             @SuppressWarnings("unchecked")
             Map<String, Filter> defaults = (Map<String, Filter>) super.createDefaults(ini, mainSection);
-            defaults.replace(DefaultFilter.authc.name(), new FormAuthenticationFilter());
-            defaults.replace(DefaultFilter.ssl.name(), new SslFilter());
-            defaults.replace(DefaultFilter.logout.name(), new LogoutFilter());
+            try {
+                // SSL filter must be first because it will trigger an exception if OmniFaces is not available
+                var sslFilter = new SslFilter();
+                defaults.replace(DefaultFilter.authc.name(), new FormAuthenticationFilter());
+                defaults.replace(DefaultFilter.ssl.name(), sslFilter);
+                defaults.replace(DefaultFilter.logout.name(), new LogoutFilter());
+            } catch (Throwable e) {
+                log.warn("unable to initialize filters", e);
+            }
             return defaults;
         }
     }

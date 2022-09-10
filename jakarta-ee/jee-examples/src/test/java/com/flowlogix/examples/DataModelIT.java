62a16cff843018026c46983446a8265d473bf909
lprimak <lenny@flowlogix.com>
2022-09-10 01:23:06 -0400

    added test from PrimeFaces data model

--- jakarta-ee/jee-examples/src/test/java/com/flowlogix/examples/DataModelIT.java
+++ jakarta-ee/jee-examples/src/test/java/com/flowlogix/examples/DataModelIT.java
@@ -16,11 +16,23 @@
 package com.flowlogix.examples;
 
 import static com.flowlogix.examples.ExceptionPageIT.DEPLOYMENT_DEV_MODE;
+import java.net.URL;
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import static org.jboss.arquillian.graphene.Graphene.guardAjax;
 import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 /**
  *
@@ -29,6 +41,33 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @ExtendWith(ArquillianExtension.class)
 @Tag("UserInterface")
 public class DataModelIT {
+    @Drone
+    private WebDriver webDriver;
+
+    @ArquillianResource
+    protected URL baseURL;
+
+    @FindBy(id = "model1")
+    private WebElement first_table;
+
+    @FindBy(id = "model1:fullNameFilter:filter")
+    private WebElement fullNameFilterInput;
+
+    @FindBy(id = "model1:0:fullName")
+    private WebElement firstRowFullName;
+
+    @Test
+    @OperateOnDeployment(DEPLOYMENT_DEV_MODE)
+    void checkPage() {
+        webDriver.get(baseURL + "view-users");
+        assertEquals("View Users", webDriver.getTitle());
+        ((JavascriptExecutor) webDriver)
+                .executeScript("arguments[0].scroll(0, 100);", first_table);
+        fullNameFilterInput.sendKeys("ly l");
+        guardAjax(fullNameFilterInput).sendKeys(Keys.RETURN);
+        assertEquals("Lovely Lady", firstRowFullName.getText());
+    }
+
     @Deployment(testable = false, name = DEPLOYMENT_DEV_MODE)
     public static WebArchive createDeployment() {
         return ExceptionPageIT.createDeploymentDev("DataModel.war");

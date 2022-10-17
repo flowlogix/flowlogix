92beca41edc2a26f9be5c7d470fcac61641e3b4f
lprimak <lenny@flowlogix.com>
2022-10-16 20:52:58 -0500

    only initialize by adding rows into db if no rows exist

--- jakarta-ee/jee-examples/src/main/java/com/flowlogix/examples/data/Initializer.java
+++ jakarta-ee/jee-examples/src/main/java/com/flowlogix/examples/data/Initializer.java
@@ -16,6 +16,7 @@
 package com.flowlogix.examples.data;
 
 import com.flowlogix.examples.entities.UserEntity;
+import com.flowlogix.jeedao.DaoHelper;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -36,18 +37,24 @@ public class Initializer {
 
     @PostConstruct
     void init() {
-        List<UserEntity> userList = Stream.of(
-                UserEntity.builder().userId("lprimak").fullName("Lenny Primak")
-                .address("Tree-Lined Blvd").zipCode(68502).build(),
-                UserEntity.builder().userId("jprimak").fullName("Lovely Lady")
-                .address("Tree-Lined Street").zipCode(68502).build(),
-                UserEntity.builder().userId("anya").fullName("Lovely Daughter")
-                .address("Tree-Lined Hill").zipCode(68502).build(),
-                UserEntity.builder().userId("friend").fullName("Friendly Pal")
-                .address("NY, Somewhere").zipCode(10012).build(),
-                UserEntity.builder().userId("cousin").fullName("Cool Cousin")
-                .address("Beastly Court").zipCode(68502).build()
-        ).collect(Collectors.toList());
-        userList.forEach(em::merge);
+        var helper = DaoHelper.<UserEntity, Long>builder()
+                .entityClass(UserEntity.class)
+                .entityManagerSupplier(() -> em)
+                .build();
+        if (helper.count() == 0) {
+            List<UserEntity> userList = Stream.of(
+                    UserEntity.builder().userId("lprimak").fullName("Lenny Primak")
+                            .address("Tree-Lined Blvd").zipCode(68502).build(),
+                    UserEntity.builder().userId("jprimak").fullName("Lovely Lady")
+                            .address("Tree-Lined Street").zipCode(68502).build(),
+                    UserEntity.builder().userId("anya").fullName("Lovely Daughter")
+                            .address("Tree-Lined Hill").zipCode(68502).build(),
+                    UserEntity.builder().userId("friend").fullName("Friendly Pal")
+                            .address("NY, Somewhere").zipCode(10012).build(),
+                    UserEntity.builder().userId("cousin").fullName("Cool Cousin")
+                            .address("Beastly Court").zipCode(68502).build()
+            ).collect(Collectors.toList());
+            userList.forEach(em::merge);
+        }
     }
 }

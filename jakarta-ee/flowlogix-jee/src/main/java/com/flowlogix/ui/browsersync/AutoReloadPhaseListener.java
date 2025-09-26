dd5b1982bc1d855b999f3b18ca44bb3ff2072b32
lprimak <lenny@flowlogix.com>
2025-09-25 19:12:14 -0500

    enh: live reload in dev mode

---
+++ jakarta-ee/flowlogix-jee/src/main/java/com/flowlogix/ui/browsersync/AutoReloadPhaseListener.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2011-2025 Flow Logix, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.flowlogix.ui.browsersync;
+
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.context.ResponseWriter;
+import jakarta.faces.context.ResponseWriterWrapper;
+import jakarta.faces.event.PhaseEvent;
+import jakarta.faces.event.PhaseId;
+import jakarta.faces.event.PhaseListener;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.omnifaces.util.Faces;
+import java.io.IOException;
+import java.io.Writer;
+
+@Slf4j
+public class AutoReloadPhaseListener implements PhaseListener {
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.RENDER_RESPONSE;
+    }
+
+    @Override
+    @SneakyThrows(IOException.class)
+    public void beforePhase(PhaseEvent event) {
+        if (!Faces.isDevelopment()) {
+            return;
+        }
+
+        FacesContext context = event.getFacesContext();
+        String encoding = context.getExternalContext().getRequestCharacterEncoding();
+        context.getExternalContext().setResponseCharacterEncoding(encoding);
+
+        ResponseWriter originalWriter = context.getRenderKit().createResponseWriter(
+                context.getExternalContext().getResponseOutputWriter(),
+                null,
+                context.getExternalContext().getRequestCharacterEncoding()
+        );
+
+        event.getFacesContext().setResponseWriter(
+                new MyResponseWriter(originalWriter, event.getFacesContext()));
+    }
+
+    private static class MyResponseWriter extends ResponseWriterWrapper {
+        private final FacesContext facesContext;
+
+        MyResponseWriter(ResponseWriter wrapped, FacesContext context) {
+            super(wrapped);
+            this.facesContext = context;
+        }
+
+        @Override
+        public ResponseWriter cloneWithWriter(Writer writer) {
+            return new MyResponseWriter(getWrapped().cloneWithWriter(writer), facesContext);
+        }
+
+        @Override
+        public void endElement(String name) throws IOException {
+            if ("body".equalsIgnoreCase(name)) {
+                String script = """
+                    <script>
+                        function connectWS() {
+                        const ws = new WebSocket('%s');
+                        ws.onmessage = e => { if (e.data === 'reload') location.reload(); };
+                        ws.onclose = () => setTimeout(connectWS, 2000);
+                        ws.onerror = () => ws.close();
+                        }
+                        connectWS();
+                    </script>
+                    """.formatted(Faces.getRequestBaseURL()
+                        .replaceFirst("^http(s)?", "ws") + "flowlogix/browsersync");
+                facesContext.getResponseWriter().write(script);
+            }
+            getWrapped().endElement(name);
+        }
+    }
+}
---
+++ jakarta-ee/flowlogix-jee/src/main/java/com/flowlogix/ui/browsersync/Configurator.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2011-2025 Flow Logix, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.flowlogix.ui.browsersync;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import lombok.extern.slf4j.Slf4j;
+import org.omnifaces.util.Faces;
+
+@Slf4j
+@WebListener
+public class Configurator implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        String disableCacheStr = sce.getServletContext().getInitParameter("com.flowlogix.faces.DISABLE_CACHE");
+        boolean disableCache = disableCacheStr == null || Boolean.parseBoolean(disableCacheStr);
+        if (Faces.isDevelopment() && disableCache) {
+            sce.getServletContext().setInitParameter("jakarta.faces.FACELETS_REFRESH_PERIOD", "0");
+        }
+    }
+}
